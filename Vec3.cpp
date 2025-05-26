fb2a8fd154f2eb65a2fa8598688ba20dcb679709
Sherman <sherman.ho@u.nus.edu>
2025-05-26 22:12:26 +0800

    Implement Vec3 class

---
+++ Vec3.cpp
@@ -0,0 +1,84 @@
+#include "Vec3.h"
+#include <cmath>
+
+Vec3::Vec3() = default;
+Vec3::Vec3(double x, double y, double z)
+	: m_x{ x }, m_y{ y }, m_z{ z }
+{
+}
+
+Vec3 Vec3::operator-() const {
+	return Vec3(-x(), -y(), -z());
+}
+
+Vec3 Vec3::operator+=(const Vec3& v) {
+	return Vec3(x() + v.x(),
+		y() + v.y(),
+		z() + v.z());
+}
+
+Vec3 Vec3::operator*=(const Vec3& v) {
+	return Vec3(x() * v.x(),
+		y() * v.y(),
+		z() * v.z());
+}
+
+Vec3 Vec3::operator/=(const Vec3& v) {
+	return Vec3(x() / v.x(),
+		y() / v.y(),
+		z() / v.z());
+}
+
+const double Vec3::length() {
+	return std::sqrt((x() * x()
+		+ y() * y()
+		+ z() * z());
+}
+
+inline std::ostream& operator<<(std::ostream& out, Vec3& v) {
+	out << v.x() << " " << v.y() << " " << v.z();
+	return out;
+}
+
+inline Vec3 operator+(Vec3& u, Vec3& v) {
+	return Vec3(u.x() + v.x(),
+		u.y() + v.y(),
+		u.z() + v.z());
+}
+
+inline Vec3 operator-(Vec3& u, Vec3& v) {
+	return Vec3(u.x() - v.x(),
+		u.y() - v.y(),
+		u.z() - v.z());
+}
+
+inline Vec3 operator*(Vec3& u, Vec3& v) {
+	return Vec3(u.x() * v.x(),
+		u.y() * v.y(),
+		u.z() * v.z());
+}
+
+inline Vec3 operator*(double t, Vec3& v) {
+	return Vec3(t * v.x(),
+		t * v.y(),
+		t * v.z());
+}
+inline Vec3 operator*(Vec3& v, double t) {
+	return operator*(t, v);
+}
+
+inline double dot(Vec3& u, Vec3& v) {
+	return (u.x() * v.x()
+		+ u.y() * v.y()
+		+ u.z() * v.z());
+}
+
+inline Vec3 cross(Vec3& u, Vec3& v) {
+	return Vec3(u.y() * v.z() - u.z() * v.y(),
+		u.z() * v.x() - u.x() * v.z(),
+		u.x() * v.y() - u.y() * v.x());
+}
+
+inline Vec3 unit_vector(Vec3& v) {
+	return v / v.length();
+}
---
+++ Vec3.h
@@ -0,0 +1,42 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+class Vec3{
+public:
+	Vec3();
+	Vec3(double x, double y, double z);
+
+	double x() const { return m_x; }
+	double y() const { return m_y; }
+	double z() const { return m_z; }
+
+	// Vector member functions
+	Vec3 operator-() const;
+	Vec3 operator+=(const Vec3& v);
+	Vec3 operator*=(const Vec3& v);
+	Vec3 operator/=(const Vec3& v);
+	double length();
+
+private:
+	double m_x;
+	double m_y;
+	double m_z;
+
+};
+
+// point3 is an alias for Vec3, useful for geometric clarity in code
+using point3 = Vec3;
+
+// Vector Utility functions
+
+inline std::ostream& operator<<(std::ostream& out, Vec3& v);
+inline Vec3 operator+(Vec3& u, Vec3& v);
+inline Vec3 operator-(Vec3& u, Vec3& v);
+inline Vec3 operator*(Vec3& u, Vec3& v);
+inline Vec3 operator*(double t, Vec3& v);
+inline Vec3 operator*(Vec3& v, double t);
+inline double dot(Vec3& u, Vec3& v);
+inline Vec3 cross(Vec3& u, Vec3& v);
+inline Vec3 unit_vector(Vec3& v);
+
+#endif // !VEC3_H
