12f14ab3e8b32fc149f7a0c8c977c764fdb847e4
Sherman <sherman.ho@u.nus.edu>
2025-05-28 14:49:54 +0800

    Add accessors and modifiers

--- HitRecord.h
+++ HitRecord.h
@@ -2,10 +2,23 @@
 #define HITRECORD_H
 
 class HitRecord {
+public:
+
+	Point3& p() const { return m_p; }
+	Vec3& normal() const { return m_normal; }
+	double t() const { return t; }
+
+	void set_p(const Point3& p);
+	void set_t(const double t);
+	void set_face_normal(const Ray& r, const Vec3& outward_normal);
+
 private:
-	Point3 p{};
-	Vec3 normal{};
-	double t{};
+	Point3 m_p{};
+	Vec3 m_normal{};
+	double m_t{};
+	bool m_front_face{};
+
+	void set_normal(const Vec3& normal);
 };
 
 #endif
